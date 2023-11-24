b3a0e5b25ee50a24cc3b4ad3c75294db089e537b
Racingwind <louis.k.hu@gmail.com>
2023-11-23 23:01:26 -0800

    now console logs an error message when response code isn't 200 success

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,19 @@
 const request = require('request');
 
 const input = process.argv.slice(2);
-const url = `https://api.thecatapii.com/v1/breeds/search?q=${input}`;
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${input}`;
 
 request(url, (error, response, body) => {
   if (error) {
     throw error;
   }
   console.log("Page fetch status: ", response && response.statusCode);
+  if (response.statusCode !== 200) {
+    console.log("An error has occured while querying the server.");
+    return;
+  }
   if (body === "[]") {
-    console.log("Cat breed not found");
+    console.log("Cat breed not found.");
     return;
   }
   const data = JSON.parse(body);
