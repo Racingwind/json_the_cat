3f6714e0fe698c54ef8e3869df87d8a5ebef7659
Racingwind <louis.k.hu@gmail.com>
2023-11-23 23:52:35 -0800

    refactored breedFetcher.js to be a module of its own and index.js to obtain user input for breed and pass it to breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,33 @@
 const request = require('request');
 
-const input = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${input}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    throw error;
-  }
-  console.log("Page fetch status: ", response && response.statusCode);
-  if (response.statusCode !== 200) {
-    console.log("An error has occured while querying the server.");
-    return;
-  }
-  if (body === "[]") {
-    console.log("Cat breed not found.");
-    return;
-  }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    // if (error) {
+    //   throw error;
+    // }
+    // console.log("Page fetch status: ", response && response.statusCode);
+    // if (response.statusCode !== 200) {
+    //   console.log("An error has occured while querying the server.");
+    //   return;
+    // }
+    // if (body === "[]") {
+    //   console.log("Cat breed not found.");
+    //   return;
+    // }
+    let desc = "";
+    console.log(response.statusCode);
+    if (body !== "[]") {
+      const data = JSON.parse(body);
+      desc = data[0].description;
+      console.log(desc);
+    } else {
+      error = "Breed not found.";
+    }
+    callback(error, desc);
+  });
+};
+
+module.exports = fetchBreedDescription;
